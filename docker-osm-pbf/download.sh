b5c5f8f4540685c395ad9e7909a09144e2e1cc9b
admire <admire@kartoza.com>
2020-01-02 17:18:49 +0200

    Added logc to check pbf download is finished

--- docker-osm-pbf/download.sh
+++ docker-osm-pbf/download.sh
@@ -3,6 +3,9 @@
 CONTINENT_LOCKFILE=/home/settings/.${CONTINENT}_lock
 COUNTRY_LOCKFILE=/home/settings/.${COUNTRY}_lock
 
+touch /home/settings/last.state.txt
+touch /home/settings/timestamp.txt
+
 # Download OSM Mapping file and Associated data
 if [ ! -f /home/settings/mapping.yml ]; then \
   wget -c ${MAPPING_URL}/mapping.yml -O /home/settings/mapping.yml
@@ -10,6 +13,14 @@ fi
 
 if [ ! -f /home/settings/qgis_style.sql ]; then \
   wget -c ${MAPPING_URL}/qgis_style.sql -O /home/settings/qgis_style.sql
+fi
+if [ ! -f /home/settings/post-pbf-import.sql ]; then \
+  url=${MAPPING_URL}/post-pbf-import.sql
+  if curl --output /dev/null --silent --head --fail "${url}"; then
+    wget -c ${MAPPING_URL}/post-pbf-import.sql -O /home/settings/post-pbf-import.sql
+  else
+    echo "URL does not exist: ${url}"
+  fi
 fi
 
 if [[ ! -f /home/settings/clip.geojson && -z ${GEOJSON_URL} ]]; then \
@@ -22,14 +33,16 @@ fi
 if [[ ! -f ${CONTINENT_LOCKFILE} && -z ${COUNTRY} ]]; then \
 
   echo "${BASE_URL}/${CONTINENT}-latest.osm.pbf"
-  wget  -c --no-check-certificate ${BASE_URL}/${CONTINENT}-latest.osm.pbf -O /home/settings/country.pbf
+  wget  -c --no-check-certificate ${BASE_URL}/${CONTINENT}-latest.osm.pbf -O /tmp/${CONTINENT}.pbf
+  mv /tmp/$CONTINENT}.pbf /home/settings/country.pbf
   touch ${CONTINENT_LOCKFILE}
 
 
 elif [[ ! -f ${COUNTRY_LOCKFILE} ]]; then
 
   echo "${BASE_URL}/${CONTINENT}/${COUNTRY}-latest.osm.pbf"
-  wget  -c --no-check-certificate ${BASE_URL}/${CONTINENT}/${COUNTRY}-latest.osm.pbf -O /home/settings/country.pbf
+  wget  -c --no-check-certificate ${BASE_URL}/${CONTINENT}/${COUNTRY}-latest.osm.pbf -O /tmp/${COUNTRY}.pbf
+  mv /tmp/${COUNTRY}.pbf /home/settings/country.pbf
   touch ${COUNTRY_LOCKFILE}
 fi
 
